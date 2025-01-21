097b3a939bf421c68a9ce679b8da755b41519838
chulp2 <chulpworker@tuta.io>
2025-01-21 17:23:13 +0300

    Day 1

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+function Accumulator(startingValue) {
+  this.value = startingValue;
+  this.read = function() {
+    this.value += +prompt("Enter value")
+  }
+}
+
+let accumulator = new Accumulator(1)
+
+accumulator.read(); // прибавляет введённое пользователем значение к текущему значению
+accumulator.read(); // прибавляет введённое пользователем значение к текущему значению
+
+alert(accumulator.value); // выведет сумму этих значений
