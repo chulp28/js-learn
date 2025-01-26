d755be945ee85ffb2d84b539c128b6ed6a7a9a14
chulp2 <chulpworker@tuta.io>
2025-01-26 23:13:43 +0300

    Day 4 evening

--- app.js
+++ app.js
@@ -1,99 +1,29 @@
-console.log(findAll(10, 3))
+//console.log(encodeRLE("aabbbccccc")); // "a2b3c5"
+//console.log(encodeRLE("aaaaaaaaax")); // "a10x"
+
+console.log(decodeRLE("a4b6c3")); // "aaaabbbbbbccc"
+console.log(decodeRLE("a10b2")); // "aaaaaaaaaabb"
+console.log(decodeRLE("abc")); // "abc"
+console.log(decodeRLE("abc11")); // "abccccccccccc"
 
 
-
-function findAll(sum, digit) {
-  let [num, store, sorted] = [[], [], []]
-  genNums(digit, num, store)
-
-  for (let i = 0; i < store.length; i++) { if(store[i].reduce((digitSum, digitValue) => digitSum + digitValue, 0) == sum) sorted.push(store[i]) }
-  return (sorted.length) ? [sorted.length, sorted.at(0).join(''), sorted.at(-1).join('')] : []
+function encodeRLE(str) {
+  let [result, lastChar, repeats] = [[], '', 0]
+  
+  for (let i = 0; i < str.length; i++) {
+    if (lastChar == '') lastChar = str[i], repeats++;
+    else {
+      if (lastChar == str[i]) repeats++;
+      else result.push(lastChar, repeats), lastChar = str[i], repeats = 1;
+      if (i == str.length - 1) result.push(lastChar, repeats);
+  }}
+  return result.filter((value) => value != 1).join('')
 }
 
-function genNums(digit, num, store, i = 0, k = 1) {
-  if (i >= digit) store.push(num.slice());
-  else {for (let j = k; j < 10; j++) { num[i] = j; if (i < digit) genNums(digit, num, store, i + 1, k), k++}};
+
+
+function decodeRLE(str) {
+  let result = str.split()
+  let num = []
+  let num
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function findAll(sum, count) {
-//  let ceil = 10**count
-//  let [minNum, maxNum, counter] = [-1, 0, 0]
-
-//  findMin: for (let i = ceil/10; i < ceil; i++) {
-//    let str = String(i).split('')
-//    let sort = String(i).split('').sort().join('')
-
-//    if (str.includes('0') || str.join('') != sort) continue findMin;
-//    if (str.reduce((redSum, value) => +redSum + +value, 0) != sum) continue findMin;
-//    minNum = i; break
-//  }
-
-//  if (minNum == -1) return []
-
-//  loop: for (let i = minNum; i < ceil; i += 9) {
-//    let str = String(i).split('')
-//    let sort = String(i).split('').sort().join('')
-    
-//    if (str.includes('0') || str.join('') != sort) continue loop;
-//    if (str.reduce((redSum, value) => +redSum + +value, 0) != sum) continue loop;
-//    maxNum = i; counter++;
-//  }
-
-//  return (counter != 0) ? [counter, String(minNum), String(maxNum)] : []
-//}
