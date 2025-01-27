22bc33b2bb109ffde83ef406a9374424f0fbb3bd
chulp2 <chulpworker@tuta.io>
2025-01-27 19:43:19 +0300

    Day 5 Evening

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
-//console.log(encodeRLE("aabbbccccc")); // "a2b3c5"
-//console.log(encodeRLE("aaaaaaaaax")); // "a10x"
-
-console.log(decodeRLE("a4b6c3")); // "aaaabbbbbbccc"
-console.log(decodeRLE("a10b2")); // "aaaaaaaaaabb"
-console.log(decodeRLE("abc")); // "abc"
-console.log(decodeRLE("abc11")); // "abccccccccccc"
-
-
-function encodeRLE(str) {
-  let [result, lastChar, repeats] = [[], '', 0]
-  
-  for (let i = 0; i < str.length; i++) {
-    if (lastChar == '') lastChar = str[i], repeats++;
-    else {
-      if (lastChar == str[i]) repeats++;
-      else result.push(lastChar, repeats), lastChar = str[i], repeats = 1;
-      if (i == str.length - 1) result.push(lastChar, repeats);
-  }}
-  return result.filter((value) => value != 1).join('')
+class Rabbit extends Object {
+  constructor(name) {
+    super()
+    this.name = name;
+  }
 }
 
+let rabbit = new Rabbit("Кроль");
 
-
-function decodeRLE(str) {
-  let result = str.split()
-  let num = []
-  let num
-}
+alert( rabbit.hasOwnProperty('name') );
