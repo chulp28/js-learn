7bbf71b97300190408c8deacd143b8f2f9cfca51
chulp2 <chulpworker@tuta.io>
2025-01-28 22:01:39 +0300

    Day 6 Evening

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-class Rabbit extends Object {
-  constructor(name) {
-    super()
-    this.name = name;
-  }
+let table = document.getElementById('age-table')
+let td = document.getElementsByTagName('td')[0].textContent
+let lables = document.getElementsByName('age')
+let searchForm = document.getElementsByName('search')[0]
+let searchFormInputs = searchForm.getElementsByTagName('input')
+console.log(lables)
+console.log(td)
+console.log(searchForm)
+console.log(searchFormInputs[0])
+console.log(searchFormInputs[1])
+//lables.style.backgroundColor = 'red'
+
+for (elem of lables) {
+  console.log(elem.value)
 }
-
-let rabbit = new Rabbit("Кроль");
-
-alert( rabbit.hasOwnProperty('name') );
