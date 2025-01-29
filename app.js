49f035aba085e8a925aec45df4ec251496f59be1
chulp2 <chulpworker@tuta.io>
2025-01-29 21:59:02 +0300

    clock

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
-let cal = document.createElement('table')
-createCalendar(cal, 2025, 0)
-
-document.body.append(cal)
-
-function createCalendar(elem, year, month) {
-	// header
-	let [trh, days] = [document.createElement('tr'), ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']];
-	for (day of days) {
-		let th = document.createElement('th'); th.textContent = day;
-		if (day == 'СБ' || day == 'ВС') th.className = 'weekend';
-		trh.append(th);
-	} elem.append(trh);
-	
-	// calendar body
-	let [buffer, calBody, date] = [[], [], new Date(year, month)];
-	let [skip, count] = [[date.getDay() - 1], new Date (((new Date(date.setMonth(date.getMonth() + 1)))).setDate(0)).getDate()];
-	for (let i = 1; i <= count;) {
-		if (skip > 0) buffer.push(''), skip--;
-		else buffer.push(i), i++;
-		if (buffer.length == 7) calBody.push(buffer.slice()), buffer.length = 0;
-	};
-	while (buffer.length < 7) buffer.push('');
-	calBody.push(buffer);
-
-	// table body
-	for (week of calBody) {
-		let tr = document.createElement('tr');
-		for (day of week) {
-			let td = document.createElement('td'); td.textContent = day;
-			tr.append(td);
-		}
-		elem.append(tr);
+class Clock {
+	currentTime = [0,0,0]
+	update() {
+		let now = new Date()
+		this.currentTime = [now.getHours(), now.getMinutes(), now.getSeconds()]
 	}
+}
+
+
+let hours = document.querySelector('.hours');
+let minutes = document.querySelector('.minutes');
+let seconds = document.querySelector('.seconds');
+
+let clock = new Clock
+
+updateHTML()
+let clockTimer = setInterval(() => updateHTML(), 1000)
+
+
+function updateHTML() {
+	clock.update()
+	hours.textContent = clock.currentTime[0]
+	minutes.textContent = clock.currentTime[1]
+	seconds.textContent = clock.currentTime[2]
 }
